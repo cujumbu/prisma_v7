ce27e648a9bc5a492eb890b05cc4d98144fe69e8
cujumbu <nethandelskompagniet@gmail.com>
2024-10-14 15:13:02 +0200

    Initial commit

---
+++ src/lib/mockDataService.ts
@@ -0,0 +1,14 @@
+{
+  "name": "warranty-claim-portal",
+  "private": true,
+  "version": "0.1.0",
+  "type": "module",
+  "scripts": {
+    "dev": "vite",
+    "build": "tsc && vite build",
+    "lint": "eslint . --ext ts,tsx --report-unused-disable-directives --max-warnings 0",
+    "preview": "vite preview",
+    "start": "node server.js"
+  },
+  // ... rest of the file remains the same
+}
